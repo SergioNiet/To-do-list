10f376a81a39234dc8fc051bd10b4c8e2f8da209
Sergio Nieto <cdmsergionieto@gmail.com>
2022-06-28 16:49:18 +0200

    aplicacion todo-list

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import './style.css';
+
+import { Todo, TodoList } from './classes'; 
+import { crearTodoHTML } from './js/componentes'; 
+
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( crearTodoHTML );
+
+
+
